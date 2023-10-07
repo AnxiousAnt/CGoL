99fca547265e7da6f1d990d29c5d4a6ac5dc3066
AnxiousAnt <92052531+AnxiousAnt@users.noreply.github.com>
2023-10-07 21:27:44 +0530

    add grid

--- main.py
+++ main.py
@@ -19,16 +19,34 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
 
+def draw_grid(positions):
+    for position in positions:
+        col, row = position
+        top_left = (col*TILE_SIZE, row*TILE_SIZE)
+        pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE))
+
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen, BLACK, (0, row*TILE_SIZE), (WIDTH, row*TILE_SIZE))
+
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen, BLACK, (col*TILE_SIZE, 0), (col*TILE_SIZE, HEIGHT))    
+
 def main():
     running = True
     
+    positions = set()
+    positions.add((10,10))
     while running:
         clock.tick(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
+        
+        screen.fill(GREY)
+        draw_grid(positions)
+        pygame.display.update()
+
     pygame.quit()
 
 if __name__ == "__main__":
