6e2049769c468e46da1f3be4fbf1dd89647770bc
AnxiousAnt <92052531+AnxiousAnt@users.noreply.github.com>
2023-10-07 21:50:02 +0530

    handle other keypresses.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -19,6 +20,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
 
+def gen(num):
+    return set([(random.randrange(0, GRID_HEIGHT), random.randrange(0, GRID_WIDTH)) for _ in range(num)])
+
 def draw_grid(positions):
     for position in positions:
         col, row = position
@@ -33,7 +37,8 @@ def draw_grid(positions):
 
 def main():
     running = True
-    
+    playing = False
+
     positions = set()
     while running:
         clock.tick(FPS)
@@ -52,6 +57,17 @@ def main():
                     positions.remove(pos)
                 else:
                     positions.add(pos)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playing = not playing
+
+                if event.key == pygame.K_c:
+                    positions = set()
+                    playing = False
+                
+                if event.key == pygame.K_g:
+                    positions = gen(random.randrange(2, 5)*GRID_WIDTH)
         
         screen.fill(GREY)
         draw_grid(positions)
